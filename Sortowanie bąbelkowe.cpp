352ccbb77c852ae706348fffa19a228fe0b0718b
kuba4444b <145668831+kuba4444b@users.noreply.github.com>
2024-04-04 10:20:16 +0200

    Update Sortowanie bąbelkowe.cpp

--- Sortowanie bąbelkowe.cpp
+++ Sortowanie bąbelkowe.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-void bubbleSort(vector<int> &arr) {
-    int n = arr.size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = 1; j < n - i; ++j) {
-            if (arr[j - 1] > arr[j]) {
-                swap(arr[j - 1], arr[j]);
+void bubbleSort(vector<int>& arr) { // Funkcja sortująca wektor liczb całkowitych metodą sortowania bąbelkowego
+    int n = arr.size(); // Rozmiar wektora
+    for (int i = 0; i < n; ++i) { // Pętla iterująca przez wszystkie elementy wektora
+        for (int j = 1; j < n - i; ++j) { // Pętla iterująca wewnątrz, porównująca sąsiednie elementy
+            if (arr[j - 1] > arr[j]) { // Jeśli obecny element jest większy od następnego
+                swap(arr[j - 1], arr[j]); // Zamień miejscami obecny i następny element
             }
         }
     }
 }
 
 int main() {
-    vector<int> arr = {21, 57, 54, 92, 89, 24, 99, 19, 20, 63};
+    vector<int> arr = { 21, 57, 54, 92, 89, 24, 99, 19, 20, 63 }; // Inicjalizacja wektora liczb całkowitych
 
-    cout << "Tablica przed sortowaniem:\n";
-    for (int num : arr) {
-        cout << num << " | ";
-    }
+    cout << "Tablica przed sortowaniem:\n"; // Wyświetlenie zawartości wektora przed sortowaniem
+    for (int num : arr) cout << num << " | ";
     cout << endl;
 
-    cout << "\nRozpoczecie sortowania...\n";
-    bubbleSort(arr);
+    bubbleSort(arr); // Wywołanie funkcji sortującej
 
-    cout << "\nTablica po sortowaniu:\n";
-    for (int num : arr) {
-        cout << num << " | ";
-    }
+    cout << "\nTablica po sortowaniu:\n"; // Wyświetlenie zawartości wektora po sortowaniu
+    for (int num : arr) cout << num << " | ";
     cout << endl;
 
     return 0;
