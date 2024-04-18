e132311b62ed9f388a8dbda3e869940300ac2525
kuba4444b <145668831+kuba4444b@users.noreply.github.com>
2024-04-18 10:32:02 +0200

    Update Ciąg Fibonacciego.cpp

--- Ciąg Fibonacciego.cpp
+++ Ciąg Fibonacciego.cpp
@@ -7,11 +7,8 @@ int main() {
 
     int f0 = 0, f1 = 1;
 
-    for (int i = 0; i < n; ++i) { //wykonanie tego 0 1 1 2 3 5 8...
+    for (int i = 0; i < n; ++i, f1 += f0, f0 = f1 - f0) {
         std::cout << f0 << std::endl;
-        int next_fib = f0 + f1;
-        f0 = f1;
-        f1 = next_fib;
     }
 
     return 0;
