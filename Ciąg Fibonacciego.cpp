7d57eef3fc5410e59dcd38906a44923b1cdf494a
kuba4444b <145668831+kuba4444b@users.noreply.github.com>
2024-04-25 10:24:03 +0200

    Update Ciąg Fibonacciego.cpp

--- Ciąg Fibonacciego.cpp
+++ Ciąg Fibonacciego.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> n;
 
     int f0 = 0, f1 = 1;
-
+    // Pierwszy wyraz jest równy 0, drugi jest równy 1, każdy następny jest sumą dwóch poprzednich
     for (int i = 0; i < n; ++i, f1 += f0, f0 = f1 - f0) {
         cout << f0 << endl;
     }
